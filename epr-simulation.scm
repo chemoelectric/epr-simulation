652ef0ba5a570f925e8f9940b3aa6e2be1676661
Barry Schwartz <sortsmill@crudfactory.com>
2023-11-26 12:44:09 -0600

    Start again with a <photon> type.

--- epr-simulation.scm
+++ epr-simulation.scm
@@ -26,15 +26,27 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 (define-library (epr-simulation)
 
-  (export π/8 π/4 π3/8 π/2 π)
-  (export HV-source)
+  (export π/180 π/8 π/4 π3/8 π/2 π)
+  (export degrees->radians radians->degrees)
+  (export <photon>
+          make-photon
+          photon?
+          photon-polarization-angle)
 
   (import (scheme base)
           (scheme inexact)
-          (srfi 27))                    ; Random bits.
+          (scheme write))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          (srfi 27))                    ; Random bits.
+
+  (cond-expand
+    (chicken (import (only (chicken base) define-record-printer)))
+    (else))
 
   (begin
 
+    ;; OEIS A019685
+    (define π/180 0.0174532925199432957692369076848861271344287188854172545609719144017100911460344944368224156963450948)
+    
     ;; OEIS A019675
     (define π/8 0.392699081698724154807830422909937860524646174921888227621868074038477050785776124828504353167764633)
 
@@ -48,12 +60,36 @@ OTHER DEALINGS IN THE SOFTWARE.
     (define π/2 1.57079632679489661923132169163975144209858469968755291048747229615390820314310449931401741267105853)
 
     ;; OEIS A000796
-    (define π   3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214)
+    (define π 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214)
 
-    (define (HV-source)
-      ;; Returns either `(0 ,π/2) or `(,π/2 0), to represent pairs of
-      ;; plane-polarized light pulses (photons), such as produced by
-      ;; radiative cascade in calcium.
-      (if (zero? (random-integer 2)) `(0 ,π/2) `(,π/2 0)))
+    (define (degrees->radians x) (* x π/180))
+    (define (radians->degrees x) (/ x π/180))
+
+    (define-record-type <photon>
+      ;; A photon is a light pulse of unit intensity. It has some
+      ;; polarization angle orthogonal to its direction of motion. The
+      ;; polarization angle is given by a real number.
+      (%%make-photon angle)
+      photon?
+      (angle photon-polarization-angle))
+
+    (define (make-photon angle)
+      (unless (real? angle)
+        (error "make-photon: expected a real number" angle))
+      (%%make-photon angle))
+
+    (cond-expand
+      (chicken (define-record-printer (<photon> rectype port)
+                 (display "<photon π×" port)
+                 (let* ((angle (photon-polarization-angle rectype))
+                        (angle/π (/ angle π)))
+                   (write angle/π port))
+                 (display ">" port)))
+      (else))
 
     )) ;; end library (epr-simulation)
+
+;; (import (epr-simulation))
+;; (import (format))
+;; (format #t "~A~%" (make-photon π3/8))
+;; (format #t "~A~%" (- (/ π 180) π/180))
